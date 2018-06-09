6cd265b25d54f7e07ce7a1d1f8a0e6e8c30347ea
이기완 <kiwan0930@gmail.com>
2018-06-09 10:09:05 +0900

    no message

---
+++ HowlTalk/LaunchScreen\.swift
@@ -0,0 +1,35 @@
+/
+//  LaunchScreen\.swift
+//  HowlTalk
+//
+//  Created by 이기완 on 09/06/2018.
+//  Copyright © 2018 이기완. All rights reserved.
+//
+
+import UIKit
+
+class LaunchScreen_: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
